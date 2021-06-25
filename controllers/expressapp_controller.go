38c6e5694dc48955420070a68bffb721c037b153
pengli <justdoit.pli@gmail.com>
2021-06-25 15:17:16 +0800

    address rbac again

--- controllers/expressapp_controller.go
+++ controllers/expressapp_controller.go
@@ -43,6 +43,7 @@ type ExpressappReconciler struct {
 //+kubebuilder:rbac:groups=ibm.dev.ibm.dev,resources=expressapps,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=ibm.dev.ibm.dev,resources=expressapps/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=ibm.dev.ibm.dev,resources=expressapps/finalizers,verbs=update
+//+kubebuilder:rbac:groups=serving.knative.dev,resources=services,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
